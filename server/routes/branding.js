5f12a7b5f690facbff6ab8f258ea61ad163c76ca
Mathias Wagner <germannewsmaker@gmail.com>
2024-04-05 23:58:34 +0200

    Created the branding.js route

---
+++ server/routes/branding.js
@@ -0,0 +1,19 @@
+const app = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+const {brandingFolder} = require("../utils/file");
+
+const readBranding = (fileName) => {
+    const file = fs.readFileSync(path.join(brandingFolder, fileName));
+    return Buffer.from(file).toString('base64');
+}
+
+const logoPayload = readBranding("logo.png");
+const titlePayload = readBranding("title.png");
+const brandingPayload = require(path.join(brandingFolder, "branding.json"));
+
+app.get('/', (req, res) => {
+    res.json({logo: logoPayload, title: titlePayload, ...brandingPayload});
+});
+
+module.exports = app;

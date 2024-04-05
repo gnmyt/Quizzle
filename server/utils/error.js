dc4d20d75a5fdddff0bd0cae65949d0d0bd133e9
Mathias Wagner <germannewsmaker@gmail.com>
2024-04-05 23:59:08 +0200

    Created the error.js util

---
+++ server/utils/error.js
@@ -0,0 +1,10 @@
+module.exports.validateSchema = (res, schema, object) => {
+    const { error } = schema.validate(object, { errors: { wrap: { label: "" } } });
+    const message = error?.details[0].message || "No message provided";
+
+    if (error && res) {
+        res.status(400).json({ message });
+    }
+
+    return error;
+};

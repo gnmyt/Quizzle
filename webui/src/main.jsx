4799e5362b47fbbf59200de4acd79549ef505acb
Mathias Wagner <germannewsmaker@gmail.com>
2024-04-06 21:33:29 +0200

    Implemented the QuizCreator page in the main.jsx

--- webui/src/main.jsx
+++ webui/src/main.jsx
@@ -4,11 +4,13 @@ import {BrandingProvider} from "@/common/contexts/Branding";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Root from "@/common/layouts/Root";
 import "@fontsource/inter";
+import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
 import "@fontsource/inter/900.css";
 import "@/common/styles/default.sass";
 import Home from "@/pages/Home";
+import QuizCreator from "@/pages/QuizCreator";
 
 const router = createBrowserRouter([
     {
@@ -16,6 +18,7 @@ const router = createBrowserRouter([
         element: <Root/>,
         children: [
             {path: '/', element: <Home/>},
+            {path: '/create', element: <QuizCreator />},
         ]
     },
 ]);

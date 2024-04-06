3c4534dec834ec6da92377837f22ac203292a0bc
Mathias Wagner <germannewsmaker@gmail.com>
2024-04-06 14:31:02 +0200

    Added animations to the Background.jsx

--- webui/src/common/components/Background/Background.jsx
+++ webui/src/common/components/Background/Background.jsx
@@ -1,10 +1,11 @@
 import "./styles.sass";
+import {motion} from "framer-motion";
 
 export const Background = () => {
     return (
         <div className="background-container">
-            <div className="circle-tl" />
-            <div className="rect-br" />
+            <motion.div className="circle-tl" animate={{left: ["-25rem", "-8rem"], top: ["-25rem", "-8rem"]}} />
+            <motion.div className="rect-br" animate={{right: ["-25rem", "-9rem"], bottom: ["-25rem", "-5rem"]}} />
         </div>
-    )
+    );
 }

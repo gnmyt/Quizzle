1629a62d133689193b072ba5a5e5d111d34be63a
Mathias Wagner <germannewsmaker@gmail.com>
2024-04-06 14:32:13 +0200

    Integrated the routers and providers into the main.jsx

--- webui/src/main.jsx
+++ webui/src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {BrandingProvider} from "@/common/contexts/Branding";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Root from "@/common/layouts/Root";
+import "@fontsource/inter";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/900.css";
+import "@/common/styles/default.sass";
+import Home from "@/pages/Home";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root/>,
+        children: [
+            {path: '/', element: <Home/>},
+        ]
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <BrandingProvider>
+            <RouterProvider router={router} />
+        </BrandingProvider>
+    </React.StrictMode>,
 )

93021f7488c9dcce325ddbb6f3f99e4894ab5e01
Mathias Wagner <germannewsmaker@gmail.com>
2024-04-06 14:14:41 +0200

    Created the Background.jsx component

---
+++ webui/src/common/components/Background/Background.jsx
@@ -0,0 +1,10 @@
+import "./styles.sass";
+
+export const Background = () => {
+    return (
+        <div className="background-container">
+            <div className="circle-tl" />
+            <div className="rect-br" />
+        </div>
+    )
+}

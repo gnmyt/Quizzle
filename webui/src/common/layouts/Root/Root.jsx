3bb8920333ec6375db6850b43e12e73c635d11c2
Mathias Wagner <germannewsmaker@gmail.com>
2024-04-06 21:28:29 +0200

    Added the Toaster and Background positions to the Root.jsx

--- webui/src/common/layouts/Root/Root.jsx
+++ webui/src/common/layouts/Root/Root.jsx
@@ -1,13 +1,17 @@
 import {Outlet} from "react-router-dom";
 import "./styles.sass";
 import Background from "@/common/components/Background";
+import {useState} from "react";
+import {Toaster} from "react-hot-toast";
 
 export const Root = () => {
+    const [circlePosition, setCirclePosition] = useState(["-25rem 0 0 -25rem", "-8rem 0 0 -8rem"]);
     return (
         <>
-            <Background />
+            <Background positionCircle={circlePosition}/>
+            <Toaster toastOptions={{position: "bottom-right"}} />
             <main>
-                <Outlet/>
+                <Outlet context={{setCirclePosition}}/>
             </main>
         </>
     );

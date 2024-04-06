e21aee7f7f6fabbb41144d990caf37c392e1091a
Mathias Wagner <germannewsmaker@gmail.com>
2024-04-06 21:27:22 +0200

    Added custom positions to the Background.jsx

--- webui/src/common/components/Background/Background.jsx
+++ webui/src/common/components/Background/Background.jsx
@@ -1,11 +1,11 @@
 import "./styles.sass";
 import {motion} from "framer-motion";
 
-export const Background = () => {
+export const Background = ({positionCircle}) => {
     return (
         <div className="background-container">
-            <motion.div className="circle-tl" animate={{left: ["-25rem", "-8rem"], top: ["-25rem", "-8rem"]}} />
-            <motion.div className="rect-br" animate={{right: ["-25rem", "-9rem"], bottom: ["-25rem", "-5rem"]}} />
+            <motion.div className="circle-tl" animate={{inset: positionCircle}}/>
+            <motion.div className="rect-br" animate={{right: ["-25rem", "-9rem"], bottom: ["-25rem", "-9rem"]}}/>
         </div>
     );
 }

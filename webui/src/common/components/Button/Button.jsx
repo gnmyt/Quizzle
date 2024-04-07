0b22201a0e715f6f1108d8694496b0ed673051d1
Mathias Wagner <germannewsmaker@gmail.com>
2024-04-07 23:50:03 +0200

    Added the disabled attribute to the Button.jsx component

--- webui/src/common/components/Button/Button.jsx
+++ webui/src/common/components/Button/Button.jsx
@@ -1,9 +1,10 @@
 import "./styles.sass";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-export const Button = ({icon, text, padding, onClick, type}) => {
+export const Button = ({icon, text, padding, onClick, type, disabled}) => {
     return (
-        <button className={`btn btn-${type ? type : "default"}`} style={{padding: padding}} onClick={onClick}>
+        <button className={`btn btn-${type ? type : "default"}`} style={{padding: padding}} onClick={onClick}
+                disabled={disabled}>
             {icon && <FontAwesomeIcon icon={icon}/>}
             {text}
         </button>

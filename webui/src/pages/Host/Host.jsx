50118e854d6c14823104dfa66457cf27ccbad118
Mathias Wagner <germannewsmaker@gmail.com>
2024-04-07 03:46:53 +0200

    Added a start button to the Host.jsx

--- webui/src/pages/Host/Host.jsx
+++ webui/src/pages/Host/Host.jsx
@@ -7,7 +7,7 @@ import Triangle from "@/pages/Host/assets/Triangle.jsx";
 import {BrandingContext} from "@/common/contexts/Branding";
 import {motion} from "framer-motion";
 import Button from "@/common/components/Button/index.js";
-import {faUser, faVolumeMute} from "@fortawesome/free-solid-svg-icons";
+import {faGamepad, faUser, faVolumeMute} from "@fortawesome/free-solid-svg-icons";
 
 export const Host = () => {
     const navigate = useNavigate();
@@ -32,17 +32,21 @@ export const Host = () => {
     return (
         <div className="host-page">
 
-            <motion.div className="quiz-information" initial={{opacity: 0, y: -100}} animate={{opacity: 1, y: 0}}>
-                <div className="info-header">
-                    <h1>“{quizRaw.title}”</h1>
-                    <QRCode value={window.location.href} size={100} renderAs="svg" className="qr"/>
-                </div>
+            <div className="quiz-info-container">
+                <motion.div className="quiz-information" initial={{opacity: 0, y: -100}} animate={{opacity: 1, y: 0}}>
+                    <div className="info-header">
+                        <h1>“{quizRaw.title}”</h1>
+                        <QRCode value={window.location.href} size={100} renderAs="svg" className="qr"/>
+                    </div>
 
-                <p>Verbinden über die Webseite <span>www.bs2ab.quiz</span> mit Code:</p>
-                <h2>0000</h2>
+                    <p>Verbinden über die Webseite <span>www.bs2ab.quiz</span> mit Code:</p>
+                    <h2>0000</h2>
 
-                <Triangle />
-            </motion.div>
+                    <Triangle/>
+                </motion.div>
+                <Button text="Starten" icon={faGamepad} padding="0.5rem 1rem" onClick={() => navigate("/host/game")}/>
+            </div>
+
 
             <motion.div className="member-info" initial={{opacity: 0, x: -100}} animate={{opacity: 1, x: 0}}>
                 <img src={titleImg} alt="Quiz Logo" className="quiz-logo"/>
@@ -51,8 +55,8 @@ export const Host = () => {
 
 
             <motion.div className="system-ui" initial={{opacity: 0, y: 100}} animate={{opacity: 1, y: 0}}>
-                <Button icon={faUser} text="0" padding="0.5rem 1rem"/>
-                <Button icon={faVolumeMute}padding="0.5rem 1rem"/>
+                <Button icon={faUser} text="0" padding="0.5rem 0.8rem"/>
+                <Button icon={faVolumeMute} padding="0.5rem 0.8rem"/>
             </motion.div>
         </div>
     );

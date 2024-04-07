7595908e594fe77ae0a1a911c7808bb038483299
Mathias Wagner <germannewsmaker@gmail.com>
2024-04-07 03:34:42 +0200

    Added the Triangle.jsx asset to the Host page

---
+++ webui/src/pages/Host/assets/Triangle.jsx
@@ -0,0 +1,9 @@
+export default () => {
+    return (
+        <svg width="157" height="137" viewBox="0 0 157 137" fill="none" xmlns="http://www.w3.org/2000/svg" className="triangle">
+            <path
+                d="M71.6946 4.12256C74.7738 -1.21078 82.4718 -1.21077 85.551 4.12256L155.266 124.873C158.345 130.206 154.496 136.873 148.338 136.873H8.90774C2.74933 136.873 -1.09965 130.206 1.97956 124.873L71.6946 4.12256Z"
+                fill="var(--primary-color)" />
+        </svg>
+    )
+}

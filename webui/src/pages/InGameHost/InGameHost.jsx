f35b404510401aac6251df2a5a5d28cd2cdfadc9
Mathias Wagner <germannewsmaker@gmail.com>
2024-04-07 23:59:49 +0200

    Implemented a leave logic to the InGameHost.jsx

--- webui/src/pages/InGameHost/InGameHost.jsx
+++ webui/src/pages/InGameHost/InGameHost.jsx
@@ -1,3 +1,32 @@
-export const InGameHost = () => {
+import {useContext, useEffect} from "react";
+import {socket} from "@/common/utils/SocketUtil.js";
+import {QuizContext} from "@/common/contexts/Quiz";
+import toast from "react-hot-toast";
+import {useNavigate} from "react-router-dom";
 
+export const InGameHost = () => {
+    const {isLoaded} = useContext(QuizContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!isLoaded) {
+            navigate("/load");
+            return;
+        }
+
+        socket.on("PLAYER_LEFT", (player) => {
+            console.log(player)
+            toast.error(`${player.name} hat das Spiel verlassen`);
+        });
+
+        return () => {
+            socket.off("PLAYER_LEFT");
+        }
+    }, [isLoaded]);
+
+    return (
+        <div>
+            <h1>Game is running</h1>
+        </div>
+    );
 }

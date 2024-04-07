d4216218e71fc7fde1e6233f553ec3b9218f3ab5
Mathias Wagner <germannewsmaker@gmail.com>
2024-04-07 23:53:32 +0200

    Integrated socket connections into the Root.jsx layout

--- webui/src/common/layouts/Root/Root.jsx
+++ webui/src/common/layouts/Root/Root.jsx
@@ -1,11 +1,21 @@
 import {Outlet} from "react-router-dom";
 import "./styles.sass";
 import Background from "@/common/components/Background";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Toaster} from "react-hot-toast";
+import {socket} from "@/common/utils/SocketUtil.js";
 
 export const Root = () => {
     const [circlePosition, setCirclePosition] = useState(["-25rem 0 0 -25rem", "-8rem 0 0 -8rem"]);
+
+    useEffect(() => {
+        socket.connect();
+
+        return () => {
+            socket.disconnect();
+        }
+    }, []);
+
     return (
         <>
             <Background positionCircle={circlePosition}/>

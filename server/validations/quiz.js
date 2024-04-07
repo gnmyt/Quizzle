be2240f1176c3c20e0aa1dd29c1409870c84300a
Mathias Wagner <germannewsmaker@gmail.com>
2024-04-07 16:07:00 +0200

    Outsourced the quiz.js validation

--- server/validations/quiz.js
+++ server/validations/quiz.js
@@ -1,14 +1,16 @@
 const Joi = require('joi');
 
+module.exports.questionValidation = Joi.object({
+    title: Joi.string().required(),
+    b64_image: Joi.string(),
+    answers: Joi.array().items(Joi.object({
+        type: Joi.string().valid('text', 'image').required(),
+        content: Joi.string().required(),
+        is_correct: Joi.boolean().required()
+    })).min(2).required()
+});
+
 module.exports.quizUpload = Joi.object({
     title: Joi.string().required(),
-    questions: Joi.array().items(Joi.object({
-        title: Joi.string().required(),
-        b64_image: Joi.string(),
-        answers: Joi.array().items(Joi.object({
-            type: Joi.string().valid('text', 'image').required(),
-            content: Joi.string().required(),
-            is_correct: Joi.boolean().required()
-        })).min(2).required()
-    })).min(1).required()
+    questions: Joi.array().items(module.exports.questionValidation).min(1).required()
 });

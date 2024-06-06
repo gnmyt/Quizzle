90b8d325f8a763c1edd2f36d4b7e8b73ebe82200
Mathias Wagner <germannewsmaker@gmail.com>
2024-06-06 09:38:05 +0200

    Created the CodeInput

---
+++ webui/src/pages/Home/components/CodeInput/CodeInput.jsx
@@ -0,0 +1,20 @@
+import CodeWrapper from "@/pages/Home/components/CodeWrapper";
+import Button from "@/common/components/Button";
+import {faQrcode} from "@fortawesome/free-solid-svg-icons";
+
+export const CodeInput = ({joinGame, errorClass}) => {
+    return (
+        <>
+            <h2>Code eingeben</h2>
+            <CodeWrapper onChange={joinGame} errorClass={errorClass}/>
+
+            <div className="alternative">
+                <hr/>
+                <h2>oder</h2>
+                <hr/>
+            </div>
+
+            <Button text="Code Scannen" icon={faQrcode} padding={"0.7rem 1.5rem"}/>
+        </>
+    )
+}
---
+++ webui/src/pages/Home/components/CodeInput/index.js
@@ -0,0 +1,1 @@
+export {CodeInput as default} from "./CodeInput.jsx";

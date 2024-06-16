3a3b7dd3275d4a943d95c0535d89b50329306a1f
Mathias Wagner <germannewsmaker@gmail.com>
2024-06-16 21:24:19 +0200

    Updated the InGameClient.jsx

--- webui/src/pages/InGameClient/InGameClient.jsx
+++ webui/src/pages/InGameClient/InGameClient.jsx
@@ -3,12 +3,17 @@ import {QuizContext} from "@/common/contexts/Quiz";
 import {useContext, useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {socket} from "@/common/utils/SocketUtil.js";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCheck, faCheckCircle, faMinus, faPaperPlane, faX} from "@fortawesome/free-solid-svg-icons";
 
 export const InGameClient = () => {
     const navigate = useNavigate();
-    const {roomCode} = useContext(QuizContext);
+    const {username, roomCode} = useContext(QuizContext);
 
-    const [currentQuestion, setCurrentQuestion] = useState({});
+    const [points, setPoints] = useState(0);
+    const [selection, setSelection] = useState([]);
+    const [currentQuestion, setCurrentQuestion] = useState(null);
+    const [answers, setAnswers] = useState([]);
 
     useEffect(() => {
         if (roomCode === null) {
@@ -17,17 +22,33 @@ export const InGameClient = () => {
         }
 
         const onQuestion = (question) => {
+            setAnswers([]);
+            setSelection([]);
             setCurrentQuestion(question);
         }
 
+        const onPoints = (points) => {
+            setPoints(points);
+        }
+
+        const onAnswer = (answer) => {
+            setAnswers(answer?.answers);
+        }
+
         socket.on("QUESTION_RECEIVED", onQuestion);
+        socket.on("POINTS_RECEIVED", onPoints);
+        socket.on("ANSWER_RECEIVED", onAnswer);
 
         return () => {
             socket.off("QUESTION_RECEIVED", onQuestion);
+            socket.off("POINTS_RECEIVED", onPoints);
+            socket.off("ANSWER_RECEIVED", onAnswer);
         }
     }, [roomCode]);
 
     const submitAnswer = (answers) => {
+        let selection = Array.from({length: currentQuestion.answers}, (_, index) => answers.includes(index));
+        setSelection(selection);
         socket.emit("SUBMIT_ANSWER", {answers}, (success) => {
             if (!success) {
                 console.error("Failed to submit answer");
@@ -37,15 +58,78 @@ export const InGameClient = () => {
         });
     }
 
+    const getCorrectStatus = (selection, answers) => {
+        let allCorrect = true;
+        let correct = 0;
+        let anySelected = false;
+
+        for (let i = 0; i < selection.length; i++) {
+            if (selection[i]) {
+                anySelected = true;
+                if (answers[i]) correct++;
+                if (!answers[i]) correct--;
+            }
+            if (answers[i] && !selection[i]) allCorrect = false;
+        }
+
+        if (allCorrect && anySelected) return 1;
+        if (correct > 0) return 0;
+
+        return -1;
+    }
+
     return (
         <div className="ingame-client">
-            <h2>Question</h2>
-            <p>{currentQuestion?.title}</p>
-            <ul>
-                {Array.from({length: currentQuestion?.answers}, (_, i) => (
-                    <button key={i} onClick={() => submitAnswer([i])}>Answer {i + 1}</button>
-                ))}
-            </ul>
+            {currentQuestion !== null && <>
+                <div className="ingame-header">
+                    <h2>{currentQuestion.title}</h2>
+                </div>
+
+                <div className="ingame-content">
+                    {currentQuestion.type === "single" && Array.from({length: currentQuestion.answers}, (_, index) => (
+                        <div key={index} className="ingame-answer" onClick={() => submitAnswer([index])}>
+                            <FontAwesomeIcon icon={faCheckCircle} className={"ingame-icon"}/>
+                        </div>
+                    ))}
+
+                    {currentQuestion.type === "multiple" && Array.from({length: currentQuestion.answers}, (_, index) => (
+                        <div key={index} className="ingame-answer" onClick={() => setSelection(prevSelection => {
+                            const newSelection = [...prevSelection];
+                            newSelection[index] = !newSelection[index];
+                            if (newSelection.filter(value => value).length > 3) return prevSelection;
+                            return newSelection;
+                        })}>
+                            <FontAwesomeIcon icon={faCheckCircle} className={"ingame-icon" + (selection[index] ? " ingame-icon-selected" : "")}/>
+                        </div>
+                    ))}
+
+                    <div className="submit-container">
+                        <button onClick={() => submitAnswer(selection.map((value, index) => value ? index : null).filter(value => value !== null)
+                    )} className={"submit-answers" + (currentQuestion.type === "multiple" && selection.some(value => value) ? " submit-shown" : "")}><FontAwesomeIcon icon={faPaperPlane}/></button></div>
+                </div>
+            </>}
+
+            {currentQuestion === null && answers.length === 0 && <div className="loading-container">
+                <div className="lds-hourglass"></div>
+            </div>}
+
+            {currentQuestion === null && answers.length > 0 && <div className="ingame-answers">
+                <FontAwesomeIcon icon={getCorrectStatus(selection, answers) === 1 ? faCheck : getCorrectStatus(selection, answers) === 0 ? faMinus : faX}
+                                 className={getCorrectStatus(selection, answers) === 1 ? " ingame-icon-correct" : getCorrectStatus(selection, answers) === 0 ? " ingame-icon-partial" : " ingame-icon-wrong"}/>
+                <h2>
+                    {getCorrectStatus(selection, answers) === 1 && "Richtig!"}
+                    {getCorrectStatus(selection, answers) === 0 && "Teilweise richtig!"}
+                    {getCorrectStatus(selection, answers) === -1 && "Falsch!"}
+                </h2>
+
+                {getCorrectStatus(selection, answers) === 0 && <h3>Richtige Antworten: {answers.map((value, index) => value ? index + 1 : null).filter(value => value !== null).join(", ")}</h3>}
+            </div>}
+            <div className="ingame-footer">
+            <h2>{username}</h2>
+                <div className="footer-points">
+                    <h2>{points}</h2>
+                </div>
+            </div>
         </div>
     );
 }

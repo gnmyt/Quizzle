46918c233180d9d9eaeb803e8ec7591451b49a13
Mathias Wagner <germannewsmaker@gmail.com>
2024-06-18 09:32:49 +0200

    Updated Scoreboard.jsx

--- webui/src/pages/InGameHost/components/Scoreboard/Scoreboard.jsx
+++ webui/src/pages/InGameHost/components/Scoreboard/Scoreboard.jsx
@@ -1,16 +1,23 @@
 import "./styles.sass";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faForward, faUser} from "@fortawesome/free-solid-svg-icons";
+import {faForward, faHouse, faUser} from "@fortawesome/free-solid-svg-icons";
 import Button from "@/common/components/Button";
+import {useNavigate} from "react-router-dom";
+import {socket} from "@/common/utils/SocketUtil.js";
 
-export const Scoreboard = ({scoreboard, nextQuestion}) => {
+export const Scoreboard = ({scoreboard, nextQuestion, isEnd}) => {
+    const goHome = () => {
+        location.reload();
+    }
     return (
         <div className="scoreboard">
             <div className="top-area">
-                <Button onClick={nextQuestion} text="Weiter"
-                        padding="1rem 1.5rem" icon={faForward}/>
+                {!isEnd && <Button onClick={nextQuestion} text="Weiter"
+                        padding="1rem 1.5rem" icon={faForward}/>}
+                {isEnd && <Button onClick={goHome} text="Startseite"
+                        padding="1rem 1.5rem" icon={faHouse}/>}
             </div>
-            <h1>Scoreboard</h1>
+            <h1>{isEnd ? "Endstand" : "Scoreboard"}</h1>
 
             <div className="scoreboard-players">
                 {scoreboard.sort((a, b) => b.points - a.points).map((player, index) => (

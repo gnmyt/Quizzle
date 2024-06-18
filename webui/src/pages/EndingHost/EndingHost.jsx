7a7f8b345a4d8d3444c2bf5326a286d0e88e4a32
Mathias Wagner <germannewsmaker@gmail.com>
2024-06-18 09:32:15 +0200

    Updated EndingHost

--- webui/src/pages/EndingHost/EndingHost.jsx
+++ webui/src/pages/EndingHost/EndingHost.jsx
@@ -2,9 +2,10 @@ import "./styles.sass";
 import {useContext, useEffect} from "react";
 import {QuizContext} from "@/common/contexts/Quiz";
 import {useNavigate} from "react-router-dom";
+import Scoreboard from "@/pages/InGameHost/components/Scoreboard/index.js";
 
 export const EndingHost = () => {
-    const {isLoaded} = useContext(QuizContext);
+    const {isLoaded, scoreboard} = useContext(QuizContext);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -16,7 +17,7 @@ export const EndingHost = () => {
 
     return (
         <div className="ending-page">
-
+            <Scoreboard isEnd nextQuestion={() => {}} scoreboard={Object.values(scoreboard?.scoreboard || {})} />
         </div>
     )
 }
